3cbf998207bb7883980520ecbcbb9ec8fce2eafd
Will McCardell <33265548+willmccardell@users.noreply.github.com>
2022-12-26 17:02:00 -0600

    Simplified the day01 part 2 solution

--- 2021/day01/day01.py
+++ 2021/day01/day01.py
@@ -17,12 +17,14 @@ def process_measurements(measurements):
     processed_data = measurements
 
     #need to iterate through the list and get a count of number 
-    #of entries that are larger than their previous one, keeping
-    #in mind the off-by-one errors
+    #of entries that are larger than their previous one, across a
+    # 3 unit sliding window
     increases = 0
     for i in range(3, len(processed_data), 1):
-        curr_3m_window = processed_data[i] + processed_data[i-1] + processed_data[i-2]
-        prev_3m_window = processed_data[i-1] + processed_data[i-2] + processed_data[i-3]
+        #both windows we compare us X-1 and X-2, so simplify that.
+        core_nugget = processed_data[i-1] + processed_data[i-2]
+        curr_3m_window = processed_data[i] + core_nugget
+        prev_3m_window = core_nugget + processed_data[i-3]
         if curr_3m_window > prev_3m_window:
             increases = increases + 1
 
