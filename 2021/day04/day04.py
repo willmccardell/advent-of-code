d5b2ea48fa632cad109b9cdc747c00c87391ec74
Will McCardell <33265548+willmccardell@users.noreply.github.com>
2023-01-14 12:25:26 -0600

    Fixed winner check to actually look past the first few cells

--- 2021/day04/day04.py
+++ 2021/day04/day04.py
@@ -14,7 +14,7 @@ class Board:
         for r in range(0,self.rowCount):
             if winner == False:
                 rowWinner = True
-                for c in self.cells[r:r+self.colCount]:
+                for c in self.cells[(r*self.colCount):(r*self.colCount) + self.colCount]:
                     if c.marked == False:
                         rowWinner = False
                 if rowWinner == True:
@@ -33,7 +33,6 @@ class Board:
         return winner
 
     def score_unmarked_cells(self):
-        score = 0
         score = sum([int(c.value) for c in self.cells if c.marked == False])
         return score
 
@@ -59,7 +58,7 @@ class Cell:
         self.marked = marked
 
     def __str__(self):
-        return self.value
+        return str(self.value)
 
 def main():
     prng, boards = read_input()
@@ -67,7 +66,7 @@ def main():
 
 def process_bingo(prng, boards):
     print(prng)
-    [print(b) for b in boards]
+    #[print(b) for b in boards]
 
     # for each number of prng
     #   mark the spot if it exists in the board's cells
@@ -79,12 +78,13 @@ def process_bingo(prng, boards):
     winning_board = None
     winning_callout = 0
     
-    for callout in prng.split(','):
+    for callout in prng:
         for board in boards:
             for cell in board.cells:
                 if cell.value == callout:
                     cell.marked = True
                     break
+            print(board)
             has_winner = board.check_if_winner()
             if has_winner == True:
                 winning_board = board
@@ -92,23 +92,29 @@ def process_bingo(prng, boards):
                 break
         if has_winner == True:
             break
-    final_score = calculate_final_score(winning_board, winning_callout)
+        
+    
     if winning_board != None:
         print("Winning board found!")
         print(winning_board)
-    print(f'Your score: {final_score}')
+        print(f'Winning Callout: {winning_callout}')
+        final_score = calculate_final_score(winning_board, winning_callout)
+        print(f'Your score: {final_score}')
+    else:
+        print("No winning board found.")
 
 def calculate_final_score(board, callout):
-    final_score = board.score_unmarked_cells() * int(callout)
+    final_score = board.score_unmarked_cells() * callout
     return final_score
 
 def read_input():
-    inputFile = "sample.txt"
+    inputFile = "input.txt"
     prng = []
     boards = []
     #grab the first line that acts as the RNG
     with open(inputFile) as f:
-        prng = f.readline().strip('\n')
+        temp_prng = f.readline().strip('\n')
+        prng = list(map(int,temp_prng.split(',')))
 
         #read into the void to skip the first blank line
 
@@ -124,7 +130,7 @@ def read_input():
             split_line = line.rstrip().split()
             if current_board.colCount <= 0:
                 current_board.colCount = len(split_line)
-            [current_board.cells.append(Cell(val, False)) for val in split_line ]
+            [current_board.cells.append(Cell(int(val), False)) for val in split_line ]
             current_board.rowCount = current_board.rowCount + 1
 
     return prng, boards
