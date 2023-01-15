de651e200a2fec9febba103ead9e6ba1b23644a0
Will McCardell <33265548+willmccardell@users.noreply.github.com>
2023-01-14 18:38:39 -0600

    basic bounds for the vent map

--- 2021/day05/day05.py
+++ 2021/day05/day05.py
@@ -11,6 +11,7 @@ class point:
     def __str__(self):
         return f"({self.x},{self.y})"
 
+
 class line:
     start_point = None
     end_point = None
@@ -22,9 +23,38 @@ class line:
     def __str__(self):
         return f'{self.start_point} -> {self.end_point}'
 
+    def max_x_cardinality(self):
+        return max(self.start_point.x, self.end_point.x)
+    
+    def max_y_cardinality(self):
+        return max(self.start_point.y, self.end_point.y)
+
+class ventmap:
+    lines = []
+    x_bounds = None
+    y_bounds = None
+    vmap = None
+
+    def __init__(self,lines):
+        self.lines = lines
+        
+        #need to iterate through lines to find the max points
+        self.x_bounds = max([l.max_x_cardinality() for l in self.lines])
+        self.y_bounds = max([l.max_y_cardinality() for l in self.lines])
+        #prepopulate the vmap
+        self.vmap = [[0 for x in range(self.x_bounds)] for x in range(self.y_bounds)]
+
+    def __str__(self):
+        #for row in self.vmap:
+          
+        return ''
+
 def main():
     input = read_input()
-    [print(l) for l in input]
+    vents = ventmap(input)
+    [print(l) for l in vents.lines]
+    print(f'Max Size: {vents.x_bounds}, {vents.y_bounds}')
+    print(vents)
 
 def read_input():
     line_list = []
@@ -33,7 +63,7 @@ def read_input():
     
     with open(inputFile) as file:
         while (read_line := file.readline().rstrip()):
-            parsed_line = parse("{},{} -> {},{}", read_line)
+            parsed_line = parse("{:d},{:d} -> {:d},{:d}", read_line)
             #print(f'({parsed_line[0]},{parsed_line[1]}) -> ({parsed_line[2]},{parsed_line[3]})')
             start_point = point(parsed_line[0],parsed_line[1])
             end_point = point(parsed_line[2],parsed_line[3])
