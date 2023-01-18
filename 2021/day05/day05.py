911dd581db020c7e78e3679aa734f6006348a250
Will McCardell <33265548+willmccardell@users.noreply.github.com>
2023-01-17 20:41:41 -0600

    Print out the grid successfully. Also 4500x increase in speed

--- 2021/day05/day05.py
+++ 2021/day05/day05.py
@@ -1,4 +1,5 @@
 from parse import *
+import time
 
 class point:
     x = None
@@ -39,7 +40,39 @@ class line:
                 (p.x >= self.start_point.x and p.x <= self.end_point.x and \
                 p.y >= self.start_point.y and p.y <= self.end_point.y)     
 
-            
+    def dumbly_find_path(self):
+        path_list = []
+        path_list.append(self.start_point)
+        path_list.append(self.end_point)
+        direction_x = self.end_point.x - self.start_point.x 
+        direction_y = self.end_point.y - self.start_point.y
+
+        impulse_x = 1
+        impulse_y = 1
+
+        if direction_x < 0:
+            impulse_x = -1
+
+        if direction_y < 0:
+            impulse_y = -1
+
+        for dx in range(0,direction_x, impulse_x ):
+            dumb_point = point(self.start_point.x + dx, self.start_point.y)
+            if dumb_point != self.start_point:
+                if self.is_point_along(dumb_point):
+                    path_list.append(dumb_point)
+
+        for dy in range(0,direction_y, impulse_y):
+            dumb_point = point(self.start_point.x, self.start_point.y + dy)
+            if dumb_point != self.start_point:
+                if self.is_point_along(dumb_point):
+                    path_list.append(dumb_point)
+
+
+        return path_list
+
+    def path(self):
+        return [point(0,0)]
 
     def max_x_cardinality(self):
         return max(self.start_point.x, self.end_point.x)
@@ -60,13 +93,29 @@ class ventmap:
         self.x_bounds = max([l.max_x_cardinality() for l in self.lines])
         self.y_bounds = max([l.max_y_cardinality() for l in self.lines])
         #prepopulate the vmap
-        self.vent_map = [[0 for x in range(self.x_bounds)] for x in range(self.y_bounds)]
+        self.vent_map = [[0 for x in range(self.x_bounds + 1)] for x in range(self.y_bounds + 1)]
+
+        for line in self.lines:
+            if line.is_straight():
+                mark_grid_list =  line.dumbly_find_path()
+                for spot in mark_grid_list:
+                    self.vent_map[spot.y][spot.x] += 1
 
     def __str__(self):
         final_grid = ''
+        for row in range(0,self.y_bounds +1):
+            for col in range(0,self.x_bounds +1):
+                grid_spot = self.vent_map[row][col]
+                if grid_spot > 0:
+                    final_grid += str(grid_spot)
+                else:
+                    final_grid += '.'
+            final_grid += '\n'
+        return final_grid
+
+    def dumb_map(self):
+        final_grid = ''
         for row in range(0,self.y_bounds + 1): 
-            #to start, check if the position has a line whose ENDPOINTS matches it.
-            #I'll then figure out real lines later
             for col in range(0,self.x_bounds + 1):
                 grid_spot_value = '.'
                 p = point(col,row)
@@ -84,9 +133,17 @@ class ventmap:
 def main():
     input = read_input()
     vents = ventmap(input)
-    [print(l) for l in vents.lines]
+    #[print(l) for l in vents.lines]
     print(f'Max Size: {vents.x_bounds}, {vents.y_bounds}')
+    start = time.time()
     print(vents)
+    end = time.time()
+    duration = end - start
+    print(duration)
+
+# remove lines that aren't straight
+def purge_input(input):
+    return list(filter(lambda x: x.is_straight(),input))
 
 def read_input():
     line_list = []
