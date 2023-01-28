ef1a967dd8fa87bd412113e9c461b0e68580f6cf
Will McCardell <33265548+willmccardell@users.noreply.github.com>
2023-01-28 16:39:35 -0600

    AI has taken over.

--- 2021/day07/day07.py
+++ 2021/day07/day07.py
@@ -1,6 +1,7 @@
 import numpy as np
+import cProfile
 
-realdeal = True
+realdeal = False
 
 
 def find_least_costly_position(crabmarines):
@@ -9,15 +10,35 @@ def find_least_costly_position(crabmarines):
     fuel_cost = crabs
     lowest_fuel = None
     converge_spot = -1
-    for dest in range(0,highest_position + 1):
-        fuel_cost = abs(crabs - dest)
-        total_fuel = sum(fuel_cost)
-        if lowest_fuel == None:
-            lowest_fuel = total_fuel
-        if total_fuel < lowest_fuel:
-            lowest_fuel = total_fuel
-            converge_spot = dest
-    print(f'Convering on Spot {converge_spot} costs {lowest_fuel}')
+    #Meatbag's version
+    # for dest in range(0,highest_position + 1):
+    #     fuel_cost = abs(crabs - dest)
+    #     total_fuel = sum(fuel_cost)
+    #     if lowest_fuel == None:
+    #         lowest_fuel = total_fuel
+    #     if total_fuel < lowest_fuel:
+    #         lowest_fuel = total_fuel
+    #         converge_spot = dest
+
+    #AI's for-loop version
+    # fuel_cost = []
+    # for dest in range(highest_position + 1):
+    #     dest_cost = []
+    #     for crab in crabs:
+    #         dest_cost.append(abs(crab - dest))
+    #     fuel_cost.append(dest_cost)
+
+    # total_fuel = [sum(x) for x in fuel_cost]
+    # lowest_fuel = min(total_fuel)
+    # converge_spot = total_fuel.index(lowest_fuel)
+
+    #AI's numpy version
+    fuel_cost = np.abs(crabs[:,np.newaxis] - np.arange(highest_position+1))
+    total_fuel = np.sum(fuel_cost, axis=0)
+    lowest_fuel = np.min(total_fuel)
+    converge_spot = np.argmin(total_fuel)
+
+    print(f'Converging on Spot {converge_spot} costs {lowest_fuel}')
 
 def calc_fuel(pos,dest):
     return pos - dest
@@ -41,3 +62,4 @@ def read_input():
 
 if __name__ == '__main__':
     main()
+    #cProfile.run('main()')
