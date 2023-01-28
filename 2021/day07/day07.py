80fe600c49e8a2a95cf361a1d4f6dc37e51ac7a0
Will McCardell <33265548+willmccardell@users.noreply.github.com>
2023-01-28 15:44:09 -0600

    2021 Day07 Part 1 bruteforce

--- 2021/day07/day07.py
+++ 2021/day07/day07.py
@@ -1,10 +1,30 @@
 import numpy as np
 
-realdeal = False
+realdeal = True
+
+
+def find_least_costly_position(crabmarines):
+    highest_position = max(crabmarines)
+    crabs = np.array(crabmarines)
+    fuel_cost = crabs
+    lowest_fuel = None
+    converge_spot = -1
+    for dest in range(0,highest_position + 1):
+        fuel_cost = abs(crabs - dest)
+        total_fuel = sum(fuel_cost)
+        if lowest_fuel == None:
+            lowest_fuel = total_fuel
+        if total_fuel < lowest_fuel:
+            lowest_fuel = total_fuel
+            converge_spot = dest
+    print(f'Convering on Spot {converge_spot} costs {lowest_fuel}')
+
+def calc_fuel(pos,dest):
+    return pos - dest
 
 def main():
     input = read_input()
-    print(input)
+    find_least_costly_position(input)
 
 def read_input():
     if realdeal == False:
